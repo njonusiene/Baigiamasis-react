435f649063fe67e93fc081ac0cc29304fe71c24a
njonusiene <jonusiene.n@gmail.com>
2024-01-23 12:52:51 +0200

    Update

--- React page/src/pages/Contact.jsx
+++ React page/src/pages/Contact.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Navbar from "../components/NavBar";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div>
+    <>
+    <motion.div
+    initial={{ opacity: 0, scale: 0 }}
+    animate={{ opacity: 1, scale: 1 }}
+    exit={{ opacity: 0, scale: 0 }}
+    transition={{ ease: 'easeInOut', duration: 0.8 }}
+    >
+      <Navbar/>
       
-    </div>
+    </motion.div>
+    </>
   )
 }
 
